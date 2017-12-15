c9767ae6ec04ed975f604c5bc375e351fe936bfa
Alex Sergeenko <lockdain@gmail.com>
2017-12-16 01:22:01 +0300

    Ok
    XML parser is since doesn't work properly because i can't find a solution how to get right child node from XML with persons/

--- src/ru/lockdain/imppeople/test/Main.java
+++ src/ru/lockdain/imppeople/test/Main.java
@@ -1,17 +1,26 @@
 package ru.lockdain.imppeople.test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 import ru.lockdain.imppeople.ReadSheet;
 import ru.lockdain.imppeople.WorkbookTools;
 import ru.lockdain.imppeople.WriteSheet;
+import ru.lockdain.imppeople.utils.Analyzer;
 import ru.lockdain.imppeople.utils.FileChecker;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
 		// TODO Auto-generated method stub
 		WriteSheet ws = new WriteSheet();
 		XSSFWorkbook workbook = new XSSFWorkbook();
@@ -30,6 +39,24 @@ public class Main {
 		
 		wbTools.createCell(workbook,(short) 0, (short) 0, (short) 0);
 		wbTools.saveWorkbookToFile(workbook, "1.xlsx");
+		
+		//Test XML parser
+		//Test node <position> read from file
+		Analyzer analyzer = new Analyzer();
+		Document doc = analyzer.getDocumentFromXML("C:\\Java\\workspace\\imppeople\\xml\\inputList.xml");
+		System.out.println("1111 " + doc.getFirstChild());
+		
+		List<String> list = new ArrayList<>();
+		list = analyzer.getPositionsMap(doc);
+		
+		Object[] output = list.toArray();
+		System.out.println("OUTPUT length is: " + output.length);
+		System.out.println("list values are: ");
+		
+		for(Object me : output) {
+			System.out.println(me.toString());
+			
+		}
 		
 	}
 
--- src/ru/lockdain/imppeople/utils/Analyzer.java
+++ src/ru/lockdain/imppeople/utils/Analyzer.java
@@ -1,5 +1,21 @@
 package ru.lockdain.imppeople.utils;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 /**
  * Класс содержит методы анализа содержимого таблицы
  * @author Alex Sergeenko
@@ -8,7 +24,47 @@ package ru.lockdain.imppeople.utils;
  *
  */
 public class Analyzer {
+	/**
+	 * Возвращает корневой документ из указанного XML
+	 * @param path
+	 * @return doc
+	 * @throws ParserConfigurationException 
+	 * @throws IOException 
+	 * @throws SAXException 
+	 */
+	public Document getDocumentFromXML(String path) throws ParserConfigurationException, SAXException, IOException {
+		
+		DocumentBuilderFactory f = DocumentBuilderFactory.newInstance();
+		f.setValidating(false);
+		DocumentBuilder builder = f.newDocumentBuilder();
+		
+		Document doc = builder.parse(new File(path));	
+		
+		return doc;
+	}
 	
-//	public 
+	/**
+	 * Возвращает перечень значений узлов Position 
+	 * из заданного документа DOM
+	 * @param doc
+	 * @return map
+	 */
+	public List getPositionsMap(Document doc) {
+		
+		List<String> list = new ArrayList<>();
+		//Node node = null;
+		NodeList children = doc.getChildNodes();
+		
+		for(int i = 0; i < children.getLength(); i++) {
+			Node node = children.item(i);
+			
+			if (node.getNodeName().equals("list")){
+				System.out.println("Found a new position!");
+				list.add(node.getNodeValue());
+			}
+		}
+				
+		return list;
+	}
 
 }
